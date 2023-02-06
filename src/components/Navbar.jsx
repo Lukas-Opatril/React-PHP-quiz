f2dff24ed2b5dc7750fabc1861120a0f134f5edc
Lukáš Opatřil <oplukase@email.cz>
2023-02-06 21:44:25 +0100

    github link update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
         <a
           type="button"
           className="h-full text-white bg-[#24292F] hover:bg-[#24292F]/90  focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30"
-          href="https://github.com/Lukas-Opatril"
+          href="https://github.com/Lukas-Opatril/React-PHP-quiz"
         >
           <svg
             className="w-4 h-4 mr-2 -ml-1"
